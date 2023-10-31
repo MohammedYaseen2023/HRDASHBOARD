f43f37e07ec7edf1265a65ba97f0dede85ae83fb
MohammedYaseen2023 <144842967+MohammedYaseen2023@users.noreply.github.com>
2023-10-31 12:00:56 +0300

    send const from api

--- HRDASHBOARD/src/api.jsx
+++ HRDASHBOARD/src/api.jsx
@@ -1,13 +1,18 @@
 import Box from '@mui/material/Box';
 
-const from_d ='01/01/2023'
+// const from_d ='01/01/2023'
 
-function singleDate (){
-return (
- <Box sx={{fontWeight: 'bold', fontSize: '25px', textAlign: 'center',color: 'secondary.main'}} >
-          كشف الحضور والانصراف ليوم {from_d}</Box>
+// function singleDate (){
+// return (
+//  <Box sx={{fontWeight: 'bold', fontSize: '25px', textAlign: 'center',color: 'secondary.main'}} >
+//           كشف الحضور والانصراف ليوم {from_d}</Box>
      
-)   
-}
+// )   
+// }
+function Date1(){
+const from_d='1111111'
+return from_d
 
-export default singleDate;
+}
+// export default singleDate;
+ export default Date1;
